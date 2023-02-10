dd87c57684e20239a977693a999c67d54246f236
jenisbay <jenis.q94@gmail.com>
2023-02-10 15:17:08 +0500

    Add Localization and Night Mode

--- app/src/main/java/com/example/newsproject/MainActivity.kt
+++ app/src/main/java/com/example/newsproject/MainActivity.kt
@@ -1,6 +1,14 @@
 package com.example.newsproject
 
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.SharedPreferences
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener
+import android.content.res.Configuration
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.fragment.NavHostFragment
@@ -8,6 +16,7 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.preference.PreferenceManager
 import com.example.newsproject.databinding.ActivityMainBinding
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,7 +28,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        observeNightMode()
+
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
@@ -27,30 +36,56 @@ class MainActivity : AppCompatActivity() {
         binding.bottomNavigationView.setupWithNavController(navController)
     }
 
-    private fun checkNightMode() {
-        val isDarkTheme = PreferenceManager
-            .getDefaultSharedPreferences(this)
-            .getBoolean("darkTheme", false)
-        setNightMode(isDarkTheme)
+    override fun onResume() {
+        super.onResume()
+        observeSharedPreferences()
     }
 
-    private fun observeNightMode() {
+    override fun onPause() {
+        super.onPause()
         PreferenceManager
             .getDefaultSharedPreferences(this)
-            .registerOnSharedPreferenceChangeListener { sharedPreferences, _ ->
-                val isDarkTheme = sharedPreferences?.getBoolean("darkTheme", false)
-                setNightMode(isDarkTheme)
-            }
-        checkNightMode()
+            .unregisterOnSharedPreferenceChangeListener(sharedPreferenceChangeListener)
     }
 
-    private fun setNightMode(isDarkTheme: Boolean?) {
+
+    private val sharedPreferenceChangeListener =
+        OnSharedPreferenceChangeListener { sharedPreferences, key ->
+            Toast.makeText(this, key, Toast.LENGTH_SHORT).show()
+
+            setLocale(sharedPreferences)
+            setNightMode(sharedPreferences)
+            recreate()
+        }
+
+    private fun observeSharedPreferences() {
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        sharedPreferences.registerOnSharedPreferenceChangeListener(sharedPreferenceChangeListener)
+
+        setLocale(sharedPreferences)
+        setNightMode(sharedPreferences)
+    }
+
+
+    private fun setNightMode(sharedPreferences: SharedPreferences) {
+
+        val isDarkTheme = sharedPreferences.getBoolean("darkTheme", false)
         AppCompatDelegate.setDefaultNightMode(
-            if (isDarkTheme == true)
+            if (isDarkTheme)
                 AppCompatDelegate.MODE_NIGHT_YES
             else
                 AppCompatDelegate.MODE_NIGHT_NO
         )
+
+    }
+
+    private fun setLocale(sharedPreferences: SharedPreferences) {
+
+        val language = sharedPreferences.getString("language", "en").toString()
+        val locale = Locale(language)
+        Locale.setDefault(locale)
+        LanguageHelper.translate(this, locale)
+
     }
 
     override fun onDestroy() {
